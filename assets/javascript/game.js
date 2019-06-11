b02dff82fd91df835f5c39688c4a1525afebf060
Mackenzie <kenziecodes@gmail.com>
2019-06-11 15:10:53 -0700

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var userScore = 0
+    var wins = 0;
+    var losses = 0;
+
+    // define the random # for every crystal
+    var computerNum = Math.floor(Math.random() * 101 + 19);
+    $(".comp-num").text(computerNum);
 
 
-var score = 0;
-var wins = 0;
-var losses = 0;
+    var imgButtons = $('.gemButton');
 
-// define the random # for every crystal
-var computerNum = Math.floor(Math.random() * 101 + 19);
-$(".score").text(computerNum);
+    for (var button of imgButtons) {
+        $(button).attr('data-value', Math.floor(Math.random() * 11 + 1))
+    }
 
-var userScore = 0
+    // reset game
+    function resetGame() {
+        userScore = 0;
+        computerNum = Math.floor(Math.random() * 101 + 19);
+        $(".score").text(userScore);
+        $(".comp-num").text(computerNum);
+        for (var button of imgButtons) {
+            $(button).attr('data-value', Math.floor(Math.random() * 11 + 1))
+        }
+    }
+    //listening for clicks
+    $(".gemButton").on("click", function () {
+        console.log($(this).attr('data-value'))
+        userScore = userScore + +$(this).attr('data-value')
+        $(".score").text(userScore);
 
-var imgButtons = $('.gemButton');
-
-for(var button of imgButtons){
-    $(button).attr('data-value',Math.floor(Math.random() * 11 + 1))
-}
-
-// reset game
-function resetGame() {
-    userScore = 0;
-    computerNum = Math.floor(Math.random() * 101 + 19);
-}
-//listening for clicks
-$(".gemButton").on("click",function(){
-    console.log($(this).attr('data-value'))
-    userScore = userScore + +$(this).attr('data-value')
-    console.log(userScore)
-});
+        if (userScore === computerNum) {
+            wins++;
+            $(".wins").text(wins);
+            resetGame();
+        } else if (userScore > computerNum) {
+            losses++;
+            $(".losses").text(losses);
+            resetGame();
+        }
+    });
 
 });
